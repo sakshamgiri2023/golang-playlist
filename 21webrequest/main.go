0957f75e0ae15154d1e8c0950b39f999012708b1
sakshamgiri500 <sakshamgiri500@gmail.com>
2025-04-16 00:01:31 +0530

    tf

--- 21webrequest/main.go
+++ 21webrequest/main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("get web request in golang")
 	// PerformGetRequest()
-	PerformPostJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
+
 }
 
 func PerformGetRequest() {
@@ -39,13 +42,33 @@ func PerformPostJsonRequest() {
 	"coursename": "let's go with golang",
 	"price": 0,
 	"platform":"lcu"
-	}
-	`)
+	} `)
 	response, err := http.Post(myurl, "application/json", requestBody)
 
 	if err != nil {
 		panic(err)
 	}
+	defer response.Body.Close()
+
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:3000/postform"
+
+	data := url.Values{}
+
+	data.Add("firstname", "saksham")
+	data.Add("lastname", "Giri")
+	data.Add("email", "giri@gmail.com")
+
+	response, err := http.PostForm(myurl, data)
+
+	if err != nil {
+		panic(err)
+	}
+
 	defer response.Body.Close()
 
 	content, _ := io.ReadAll(response.Body)
