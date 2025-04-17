2efb1dda2fb86cccb1c65c45f75cf0b0932d17d2
sakshamgiri500 <sakshamgiri500@gmail.com>
2025-04-17 07:58:09 +0530

    tf

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Course struct {
 	CourseId    string  `json:"courseId"`
 	CourseName  string  `json:"coursename"`
 	CoursePrice string  `json:"price"`
-	Author      *Author `json:author`
+	Author      *Author `json:"author"`
 }
 
 type Author struct {
@@ -22,4 +30,34 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
+}
+
+//controller - file
+
+//serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>welcome to build api</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("no course found with given id")
+	return
 }
