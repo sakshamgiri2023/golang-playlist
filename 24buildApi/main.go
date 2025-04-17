324fd27fd2dc162d1b3a57fc680e55922491b234
sakshamgiri500 <sakshamgiri500@gmail.com>
2025-04-17 08:46:00 +0530

    tf

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +29,8 @@ var courses []Course
 //middleware
 
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
@@ -60,4 +64,32 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("no course found with given id")
 	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("create one course")
+	w.Header().Set("Course-Type", "application/json")
+
+	// what if: body is empty
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside json")
+		return
+	}
+
+	//geneate unique id, string
+	//append course into courses
+
+	rand.NewSource(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+
 }
